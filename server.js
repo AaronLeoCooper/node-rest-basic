fc1bb7a3acda9184aba56f9dfebdef6d214e51d6
Aaron Cooper <aaron.cooper@2359media.com>
2016-02-03 17:50:28 +0800

    Added default / route, same as /users

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+app.get('/', function(req, res) {
+  fs.readFile(__dirname + '/json/users.json', 'utf8', function(err, data) {
+    console.log(data);
+    res.end(data);
+  });
+});
+
 app.get('/users', function(req, res) {
   fs.readFile(__dirname + '/json/users.json', 'utf8', function(err, data) {
     console.log(data);
