4a0cb132b7b39e7d608aaa350a273dad2cd83a86
Aaron Cooper <aaron.cooper@2359media.com>
2016-02-03 18:09:27 +0800

    Added Procfile for Heroku settings. Added Heroku parameter to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+// Heroku's config parameter HEROKU gets set when running on Heroku, otherwise assume locally running
+var heroku = process.env.HEROKU || false;
 
 app.get('/', function(req, res) {
   fs.readFile(__dirname + '/json/users.json', 'utf8', function(err, data) {
@@ -16,9 +18,11 @@ app.get('/users', function(req, res) {
   });
 });
 
-var server = app.listen(1111, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+if ( heroku ) {
+  var server = app.listen(1111, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Test app listening at http://%s:%s', host, port);
-});
+    console.log('Test app listening at http://%s:%s', host, port);
+  });
+}
